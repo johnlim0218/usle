0a89373d8c6337f281cb300b27d1a64f58056483
johnlim0218 <gwanwoo.john@gmail.com>
2019-10-23 15:20:35 +0900

    Add Sequelize

---
+++ 2019_10_17_USLE/back/config/config.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+    "development" : {
+        "username" : "root",
+        "password" : process.env.DB_PASSWORD,
+        "database" : "usle_db",
+        "host" : "127.0.0.1",
+        "port" : "3306",
+        "dialect" : "mysql",
+        "operatorsAliases": false
+    },
+    "test" : {
+        "username" : "root",
+        "password" : process.env.DB_PASSWORD,
+        "database" : "usle_db",
+        "host" : "127.0.0.1",
+        "port" : "3306",
+        "dialect" : "mysql",
+        "operatorsAliases": false
+    },
+    "production" : {
+        "username" : "root",
+        "password" : process.env.DB_PASSWORD,
+        "database" : "usle_db",
+        "host" : "127.0.0.1",
+        "port" : "3306",
+        "dialect" : "mysql",
+        "operatorsAliases": false
+    }
+}
--- 2019_10_17_USLE/back/index.js
+++ 2019_10_17_USLE/back/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const db = require('./models');
 
 const app = express();
+db.sequelize.sync();
+
+app.use(morgan('dev'));
 
 app.listen(3065, () => {
     console.log('usle server is running on localhost:3065');
